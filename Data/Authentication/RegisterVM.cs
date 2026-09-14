d99fb4e5c466c5c796a0da08c4930ee4b4ecc297
Mark Kiraly <kmark2062@gmail.com>
2026-09-14 16:06:40 +0200

    Add 'Data/Authentication/RegisterVM' file - add a custom view model 'Register'

---
+++ Data/Authentication/RegisterVM.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fullstack_project_1.Data.Authentication
+{
+    public class RegisterVM
+    {
+        // a custom view model
+
+
+        [Required(ErrorMessage = "Username is required")]   // must provide this data, otherwise throw an error
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        //[Required(ErrorMessage = "Role is required")]
+        //public string Role { get; set; }
+
+    }
+}
