f51676f1ee5d75197ce9254c2b2e6b410b046fd1
Mark Kiraly <kmark2062@gmail.com>
2026-09-15 11:37:04 +0200

    Modify 'Data/AppDbContext' file - use Fluent API - AspNetUsers table -- Explicitly map properties to PostgreSQL column names and data types

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -16,6 +16,69 @@ namespace fullstack_project_1.Data
             this.Configuration = configuration;
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);  // with base.OnModelCreating, Identity configures default schemas
+
+            builder.Entity<AspNetUser>(entity =>
+            {
+                entity.ToTable("AspNetUsers");
+                
+                // Explicitly map properties to PostgreSQL column names and data types, (Fluent API usage)
+                entity.Property(e => e.Id)
+                    .HasColumnName("Id")
+                    .HasColumnType("varchar(450)");
+
+                entity.Property(e => e.AccessFailedCount)
+                    .HasColumnName("AccessFailedCount");
+
+                entity.Property(e => e.ConcurrencyStamp)
+                    .HasColumnName("ConcurrencyStamp");
+
+                entity.Property(e => e.Email)
+                    .HasColumnName("Email")
+                    .HasColumnType("varchar(256)");
+
+                entity.Property(e => e.EmailConfirmed)
+                    .HasColumnName("EmailConfirmed");
+
+                entity.Property(e => e.LockoutEnabled)
+                    .HasColumnName("LockoutEnabled");
+
+                entity.Property(e => e.LockoutEnd)
+                    .HasColumnName("LockoutEnd");
+
+                entity.Property(e => e.NormalizedEmail)
+                    .HasColumnName("NormalizedEmail")
+                    .HasColumnType("varchar(256)");
+
+                entity.Property(e => e.NormalizedUserName)
+                    .HasColumnName("NormalizedUserName")
+                    .HasColumnType("varchar(256)");
+
+                entity.Property(e => e.PasswordHash)
+                    .HasColumnName("PasswordHash");
+
+                entity.Property(e => e.PhoneNumber)
+                    .HasColumnName("PhoneNumber");
+
+                entity.Property(e => e.PhoneNumberConfirmed)
+                    .HasColumnName("PhoneNumberConfirmed");
+
+                entity.Property(e => e.SecurityStamp)
+                    .HasColumnName("SecurityStamp");
+
+                entity.Property(e => e.TwoFactorEnabled)
+                    .HasColumnName("TwoFactorEnabled");
+
+                entity.Property(e => e.UserName)
+                    .HasColumnName("UserName")
+                    .HasColumnType("varchar(256)");
+                
+                });
+
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //base.OnConfiguring(optionsBuilder);
