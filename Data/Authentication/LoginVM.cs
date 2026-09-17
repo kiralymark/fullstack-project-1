96f613dfd3a5c61ace231eb807dde13293560a60
Mark Kiraly <kmark2062@gmail.com>
2026-09-17 12:14:36 +0200

    Add 'Data/Authentication/LoginVM' file -- add the Login View Model

---
+++ Data/Authentication/LoginVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fullstack_project_1.Data.Authentication
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "Email is required")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public required string Password { get; set; }
+
+    }
+
+}
